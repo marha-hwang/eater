4e357f15e2152a0b07e563cda35eb0f4bfe521ed
hicksKim <lmkkjhkchk@gmail.com>
2022-03-22 21:47:12 +0900

    이거 보이면 성공

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //hihi
+        //김종휘
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
